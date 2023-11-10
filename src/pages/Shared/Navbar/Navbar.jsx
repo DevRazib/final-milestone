a9d2e91554d22d391782c2b00b9147d9a1b54e63
DevRazib <ahmedrazib477@gmail.com>
2023-11-11 00:20:57 +0600

    parallax added using react parallax

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
 
   const navOptions=<>
-    <li><Link>Home</Link></li>
+    <li><Link to='/'>Home</Link></li>
     <li><Link>Contact Us</Link></li>
     <li><Link>Dashboard</Link></li>
-    <li><Link>Our Menu</Link></li>
+    <li><Link to='/menu'>Our Menu</Link></li>
     <li><Link>Our Shop</Link></li>
   </>
   return (
